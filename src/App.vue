aa60517d671ea6e3118528bf9828c6d2fdc98610
lazojeisson <jeissonlazo@gmail.com>
2024-02-07 18:36:06 -0500

    kick start

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+<div class="common-layout">
+  <el-container>
+    <el-header>
+      <HeaderMenu></HeaderMenu>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</div>
+</template>
+
+<script lang="ts" setup>
+import HeaderMenu from "./components/HeaderMenu.vue";
+
+</script>
+<style scoped>
+.layout-container-demo .el-header {
+  position: relative;
+  background-color: var(--el-color-primary-light-7);
+  color: var(--el-text-color-primary);
+}
+.layout-container-demo .el-aside {
+  color: var(--el-text-color-primary);
+  background: var(--el-color-primary-light-8);
+}
+.layout-container-demo .el-menu {
+  border-right: none;
+}
+.layout-container-demo .el-main {
+  padding: 0;
+}
+.layout-container-demo .toolbar {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  right: 20px;
+}
+</style>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-menu
+    :default-active="activeIndex2"
+    class="headerMenu"
+    mode="horizontal"
+    @select="handleSelect"
+  >
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>Workspace</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-sub-menu index="2-4">
+        <template #title>item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="3" disabled>Info</el-menu-item>
+    <el-menu-item index="4">Orders</el-menu-item>
+    <el-menu-item>
+      <el-switch
+      v-model="value1"
+      :active-action-icon="Sunny"
+      :inactive-action-icon="Moon"
+    />
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { Sunny, Moon } from '@element-plus/icons-vue';
+const value1 = ref(true)
+// import { colors } from ''
+
+const activeIndex2 = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+<style scoped lang="scss">
+</style>
